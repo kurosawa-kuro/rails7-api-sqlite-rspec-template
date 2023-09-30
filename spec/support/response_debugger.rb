1f8e32843a3e188be76c903299e3ff56b7ef7661
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-30 09:48:27 +0000

    add

--- spec/support/response_debugger.rb
+++ spec/support/response_debugger.rb
@@ -1,5 +1,6 @@
 module ResponseDebugger
     def print_response_details
+      puts ""
       puts "Status: #{response.status}"
       puts "Headers: #{response.headers.select { |k, v| ['Content-Type', 'X-Frame-Options'].include? k }}"
       puts "Request Params: #{response.request.params.inspect}"
